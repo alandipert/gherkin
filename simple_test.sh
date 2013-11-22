cba936b0189ee238fae47ef51352ff0ce8300c01
Paul Gearon <gearon@computer.org>
2013-11-21 23:36:03 -0500

    A simple test to see if we can grep for 'bash' in all our files (except this one)

---
+++ simple_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+expected=$(
+cat << "EOF"
+("#!/usr/bin/env bash" "  echo \"bash >= 4.0.0 required\" >&2" "#!/bin/bash" "    \"how do we replace bash?\")" "    (title \"gherkin - a lisp1 written in bash4\")" "    \"  - no real array in bash\"" "    \"Aron Griffis: bash pro tips\"")
+EOF
+)
+
+actual=$(./gherkin -e '(load-file "core") (sh (str "grep -h bash " (join " " (remove (fn (x) (eq? "simple_test.sh" x)) (sh "ls")))))')
+
+if [[ "$expected" == "$actual" ]]; then
+  echo "test 1/1 passed"
+else
+  echo "test 1/1 failed"
+  echo "EXPECTED: $expected"
+  echo "RECEIVED: $actual"
+fi
